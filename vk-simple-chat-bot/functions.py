a10d905f5ef784ee201a19ef2dc27e31b94c6789
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-04-08 18:15:26 +0300

    Update

--- vk-simple-chat-bot/functions.py
+++ vk-simple-chat-bot/functions.py
@@ -1,7 +1,31 @@
-from pathlib import Path
-
 import random
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_random_file(path):
     return random.choice(tuple((path).iterdir()))
+
+def get_weather(city):
+    url = f"https://sinoptik.com.ru/погода-{city}"
+    response = requests.get(url)
+    write_html(response.content)
+    print(response.status_code)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        temp = soup.find("div", class_="weather__article_main_temp").text.strip()
+        sunrise = soup.find("div", class_="ss_wrap ru").findAll("span")[0].text
+        sunset = soup.find("div", class_="ss_wrap ru").findAll("span")[1].text
+        pressure = soup.find("div", class_="table__col current").find("div", class_="table__pressure").text
+        humidity = soup.find("div", class_="table__col current").find("div", class_="table__humidity").text
+        wind = soup.find("div", class_="table__col current").findAll("label", class_="show-tooltip")[1].text
+        weather_data = {
+            "temp": temp,
+            "sunrise": sunrise,
+            "sunset": sunset,
+            "pressure": pressure,
+            "humidity": humidity,
+            "wind": wind
+        }
+        return weather_data
--- vk-simple-chat-bot/main.py
+++ vk-simple-chat-bot/main.py
@@ -1,75 +1,113 @@
 # -*- coding: utf-8 -*-
 import random
+import logging
 from pathlib import Path
 
+import requests
 import vk_api
 from vk_api import VkUpload
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 import yaml
 
-from functions import get_random_file
+from functions import get_random_file, get_weather
 
 
-with open("config.yaml") as ymlFile:
+BASE_DIR = Path(__file__).resolve().parent
+CONFIG_PATH = BASE_DIR.joinpath("config.yaml")
+IMG_DIR = BASE_DIR.joinpath("img")
+VIDEO_DIR = BASE_DIR.joinpath("video")
+MUSIC_DIR = BASE_DIR.joinpath("music")
+DOC_DIR = BASE_DIR.joinpath("documents")
+
+# Считываем данные с конфига
+with open(CONFIG_PATH, encoding="utf-8") as ymlFile:
     config = yaml.load(ymlFile.read(), Loader=yaml.Loader)
 
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    level=logging.INFO
+)
 
-class Bot():
-    def __init__(self):
-        self.BASE_DIR = Path(__file__).resolve().parent
-        self.IMG_DIR = self.BASE_DIR.joinpath("img")
-        self.VIDEO_DIR = self.BASE_DIR.joinpath("video")
-        self.MUSIC_DIR = self.BASE_DIR.joinpath("music")
-        self.DOC_DIR = self.BASE_DIR.joinpath("documents")
+# Авторизация бота
+authorize = vk_api.VkApi(token=config["group"]["group_token"])
 
-        # Авторизация бота
-        authorize = vk_api.VkApi(token=config["group"]["group_token"])
+longpoll = VkBotLongPoll(authorize, group_id=config["group"]["group_id"])
+bot_upload = vk_api.VkUpload(authorize)
+bot = authorize.get_api()
 
-        self.longpoll = VkBotLongPoll(authorize, group_id=config["group"]["group_id"])
-        self.upload = vk_api.VkUpload(authorize)
-        self.bot = authorize.get_api()
+# Авторизация в пользователя
+vk_session = vk_api.VkApi(token=config["user"]["user_token"])
 
-        # Авторизация в vk session
-        vk_session = vk_api.VkApi(token=config["user"]["user_token"])
+vk = vk_session.get_api()
+vk_upload = vk_api.VkUpload(vk_session)
 
-        self.vk = vk_session.get_api()
-        self.vk_upload = vk_api.VkUpload(vk_session)
+logging.info("Авторизация прошла успешно")
 
+
+class Utils:
+    def get_random_member(self, chat_id):
+        """Возвращает id случайного участника беседы"""
+        members = bot.messages.getConversationMembers(
+            peer_id=2000000000 + chat_id,
+            group_id=config["group"]["group_id"]
+        )["items"]
+        member_id = random.choice(members)["member_id"]
+        return member_id
+
+    def get_username(self, user_id):
+        user_info = vk.users.get(user_ids=user_id)[0]
+        username = "{} {}".format(
+            user_info["first_name"],
+            user_info["last_name"]
+        )
+        return f"[id{user_id}|{username}]"
+
+    def get_group_name(self, group_id):
+        group_info = vk.groups.getById(group_id=-group_id)[0]
+        return f"[club{group_info['id']}|{group_info['name']}]"
+
+
+class VkBot:
     def write_message(self, message="", attachment=""):
         """Отправляем в беседу сообщение."""
-        self.bot.messages.send(
+        bot.messages.send(
             chat_id=self.chat_id,
             message=message,
             attachment=attachment,
             random_id=get_random_id()
         )
 
-    def say_hello(self):
-        user_info = self.vk.users.get(user_id=self.user_id)[0]
+    def say_hello(self, user_id):
+        user_info = vk.users.get(user_id=user_id)[0]
         username = user_info["first_name"]
         self.write_message(message=f"Привет {username}!")
 
     def send_file(self, file, file_type):
+        attachment = ""
         if file_type == "photo":
             """Загружаем фото на сервер Вконтакте."""
-            response = self.upload.photo_messages(photos=file)[0]
+            response = bot_upload.photo_messages(
+                photos=file,
+                peer_id=2000000000 + self.chat_id
+            )[0]
             attachment = "photo{}_{}".format(response["owner_id"], response["id"])
         elif file_type == "video":
             """Загружаем видео на сервер Вконтакте."""
-            response = self.vk_upload.video(video_file=file, name='test')
+            response = vk_upload.video(video_file=file, name="Видео")
             attachment = "video{}_{}".format(response["owner_id"], response["video_id"])
         elif file_type == "audio":
             """Загружаем аудиозапись на сервер Вконтакте."""
             song_data = str(file.name)[:-3].split(" - ")
-            response = self.vk_upload.audio(
+            response = vk_upload.audio(
                 audio = str(file),
                 artist = song_data[0],
                 title = song_data[1]
             )
             attachment = "audio{}_{}".format(response["owner_id"], response["id"])
         elif file_type == "doc":
-            response = self.upload.document_message(
+            response = bot_upload.document_message(
                 doc=file,
                 title="doc",
                 peer_id=2000000000 + self.chat_id
@@ -77,42 +115,93 @@ class Bot():
             attachment = "doc{}_{}".format(response["owner_id"], response["id"])
         self.write_message(attachment=attachment)
 
-    # # Не работает
-    # def send_doc(self, file):
-    #     response = self.vk_upload.document(doc=file)["doc"]
-    #     attachment = "doc{}_{}".format(response["owner_id"], response["id"])
-    #     self.write_message(attachment=attachment)
-
     def check_message(self, received_message):
         if received_message == "привет":
-            self.say_hello()
-        elif received_message == "картинка":
-            photo = get_random_file(self.IMG_DIR)
-            self.send_file(str(photo), "photo")
-        elif received_message == "видео":
-            video = get_random_file(self.VIDEO_DIR)
-            self.send_file(str(video), "video")
-        elif received_message == "аудио":
-            audio = get_random_file(self.MUSIC_DIR)
-            self.send_file(audio, "audio")
-        elif received_message == "документ":
-            document = get_random_file(self.DOC_DIR)
-            self.send_file(str(document), "doc")
+            username = vk.users.get(user_id=self.sender_id)[0]["first_name"]
+            message = f"Привет {username}!"
+            self.write_message(message=message)
 
-    def run(self):
+        elif received_message == "фото":
+            photo = get_random_file(IMG_DIR)
+            self.send_file(
+                file=str(photo),
+                file_type="photo"
+            )
+
+        elif received_message == "видео":
+            video = get_random_file(VIDEO_DIR)
+            self.send_file(
+                file=str(video),
+                file_type="video"
+            )
+
+        elif received_message == "аудио":
+            audio = get_random_file(MUSIC_DIR)
+            self.send_file(
+                file=audio,
+                file_type="audio"
+            )
+
+        elif received_message == "документ":
+            document = get_random_file(DOC_DIR)
+            self.send_file(
+                file=str(document),
+                file_type="doc"
+            )
+
+        elif received_message[:3] == "кто":
+            member_id = utils.get_random_member(chat_id=self.chat_id)
+            phrases = ["Я думаю, это ", "Однозначно это ", "Скорее всего, это ", "Это ты"]
+            message = random.choice(phrases)
+            if message != "Это ты":
+                if member_id > 0:
+                    message += utils.get_username(member_id)
+                else:
+                    message += utils.get_group_name(member_id)
+            self.write_message(message)
+
+        elif received_message[:6] == "погода":
+            city = received_message[7:].lower().replace(" ", "-")
+            weather_data = get_weather(city)
+
+            print(weather_data)
+
+            if weather_data:
+                city = city[:1].upper() + city[1:]
+                message = f"""
+{city}
+Температура {weather_data['temp']} 🌡
+Восход {weather_data['sunrise']} 🌅
+Закат {weather_data['sunset']} 🌇
+Давление {weather_data['pressure']} мм
+Влажность {weather_data['humidity']} %
+Ветер {weather_data['wind']} м/c
+"""
+            else:
+                message = "Город не найден"
+
+            self.write_message(message=message)
+
+    def listen(self):
         """Отслеживаем каждое событие в беседе."""
         while True:
             try:
-                for event in self.longpoll.listen():
+                for event in longpoll.listen():
                     if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat and event.message.get("text") != "":
                         received_message = event.message.get("text").lower()
                         self.chat_id = event.chat_id
-                        self.user_id = event.message.get("from_id")
+                        self.sender_id = event.message.get("from_id")
                         self.check_message(received_message)
-            except Exception as e:
+            except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
+                # Перезагрузка серверов ВКонтакте
                 print(e)
+                logging.info("Перезапуск бота")
 
+    def run(self):
+        logging.info("Бот запущен")
+        self.listen()
 
 if __name__ == "__main__":
-    VkBot = Bot()
-    VkBot.run()
+    vkbot = VkBot()
+    utils = Utils()
+    vkbot.run()
